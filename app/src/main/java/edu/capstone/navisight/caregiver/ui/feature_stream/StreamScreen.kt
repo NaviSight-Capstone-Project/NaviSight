a106222f29d281f2f769bbeb215cc95a59fc01b5
fraeron <rafaeljtenido@gmail.com>
2025-12-02 12:57:56 +0800

    Revamp UI of Live Call/StreamScreen.kt

--- app/src/main/java/edu/capstone/navisight/caregiver/ui/feature_stream/StreamScreen.kt
+++ app/src/main/java/edu/capstone/navisight/caregiver/ui/feature_stream/StreamScreen.kt
@@ -3,11 +3,14 @@ package edu.capstone.navisight.caregiver.ui.feature_stream
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +19,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +32,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,6 +50,7 @@ import edu.capstone.navisight.caregiver.model.Viu
 import edu.capstone.navisight.common.webrtc.adapter.UserListView
 import com.google.firebase.auth.FirebaseAuth
 import edu.capstone.navisight.common.webrtc.FirebaseClient
+import kotlinx.coroutines.launch
 
 @Composable
 fun StreamScreen(
@@ -50,10 +58,10 @@ fun StreamScreen(
     // Init. callbacks for MainActivity
      onVideoCall: (username: String) -> Unit,
      onAudioCall: (username: String) -> Unit){
-    val usersList = remember { mutableStateListOf<Triple<String, String, String>>() }
+    val usersList = remember { mutableStateListOf<Triple<Viu, String, String>>() }
     val firebaseClient = FirebaseClient.getInstance()
 
-    // NEW STATE: Holds the list of VIU UIDs associated with the caregiver
+    // Hold the list of VIU UIDs associated with the caregiver
     var associatedViuUids by remember { mutableStateOf<List<String>?>(null) }
 
     // Get the current caregiver UID (MUST be non-null if they're signed in)
@@ -63,14 +71,14 @@ fun StreamScreen(
         return
     }
 
-    // --- STEP 1: Fetch Associated VIU UIDs from Firestore ---
+    // Fetch Associated VIU UIDs from Firestore
     LaunchedEffect(key1 = caregiverUid) {
         // This runs ONCE when the screen loads
         val uids = firebaseClient.getAssociatedViuUids(caregiverUid)
         associatedViuUids = uids // Update state, triggers the next LaunchedEffect
     }
 
-    // --- STEP 2: Observe RTDB only when UIDs are available ---
+    //  Observe RTDB only when UIDs are available
     LaunchedEffect(key1 = associatedViuUids) {
         val viusToObserve = associatedViuUids
 
@@ -84,8 +92,7 @@ fun StreamScreen(
                     .filter { (viu, _) -> viu != null }
                     .mapNotNull { (viu, status) ->
                         val uid = viu?.uid ?: return@mapNotNull null
-                        val firstName = viu.firstName
-                        Triple(firstName, uid, status)
+                        Triple(viu, uid, status)
                     }
 
                 usersList.clear()
@@ -94,124 +101,194 @@ fun StreamScreen(
         }
     }
 
+    data class Item(val id: Int, val name: String)
+
+    val rawDataFromSource = remember {
+        listOf(
+            Item(1, "Dog"),
+            Item(2, "Apple"),
+            Item(3, "Frog"),
+            Item(4, "Banana")
+        )
+    }
+
+    val yourSortedItemList = remember {
+        rawDataFromSource.sortedBy { it.name }
+    }
+
+
+    val listState = rememberLazyListState()
+
+
+    LazyColumn(state = listState) {
+        items(yourSortedItemList) { item ->
+            Text(item.name) // Or your custom item composable
+        }
+    }
+
+    val scope = rememberCoroutineScope()
+    val alphabet = ('A'..'Z').toList()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFEFF0F1))
             .windowInsetsPadding(WindowInsets.statusBars)
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+            .padding(horizontal = 4.dp, vertical = 12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Header. So fantastic. So wow.
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(horizontal = 12.dp)
+                .fillMaxSize(), // Occupy the whole screen area
+
+            horizontalArrangement = Arrangement.SpaceBetween // Pushes the two main children apart
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_stream),
-                contentDescription = "Stream Icon",
-                tint = Color(0xFF6041EC),
-                modifier = Modifier.size(28.dp)
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                text = "Live Video and Audio Call",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium,
-                color = Color(0xFF202833),
-                style = TextStyle(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(Color(0xFFB644F1), Color(0xFF6041EC))
-                    )
-                )
-            )
-        }
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-
-        // Control
-        if (associatedViuUids == null) {
-            // Maghintay ka.
-            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
+            // Set header with main content (this is compartmentalized for alphabet scrollbar)
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                // Set header
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    CircularProgressIndicator()
-                    Spacer(modifier = Modifier.height(25.dp))
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_stream),
+                        contentDescription = "Stream Icon",
+                        tint = Color(0xFF6041EC),
+                        modifier = Modifier.size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
                     Text(
-                        textAlign = TextAlign.Center,
-                        text = "Fetching the status of your VIUs.\nPlease wait!",
-                        style = MaterialTheme.typography.bodyLarge.copy(
-                            fontWeight = FontWeight.Medium,
-                            color = Color.Gray
+                        text = "Live Video and Audio Call",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color(0xFF202833),
+                        style = TextStyle(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(Color(0xFFB644F1), Color(0xFF6041EC))
+                            )
                         )
                     )
                 }
-            }
 
-        } else if (usersList.isEmpty() && associatedViuUids?.isNotEmpty() == true) {
-            // Show a message if relationships exist, but no one is online
-            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Spacer(modifier = Modifier.height(12.dp))
 
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_no_internet),
-                        contentDescription = "Icon",
-                        modifier = Modifier.size(120.dp),
-                        colorFilter = ColorFilter.tint(Color.Gray)
-                    )
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Text(
-                        textAlign = TextAlign.Center,
-                        text = "Can't fetch your VIUs at the moment.\nTry again later?",
-                        style = MaterialTheme.typography.bodyLarge.copy(
-                            fontWeight = FontWeight.Medium,
-                            color = Color.Gray
+                // Set main content with controls
+                if (associatedViuUids == null) {
+                    // Show when waiting
+                    Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator()
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Text(
+                                textAlign = TextAlign.Center,
+                                text = "Fetching the status of your VIUs.\nPlease wait!",
+                                style = MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Gray
+                                )
+                            )
+                        }
+                    }
+
+                } else if (usersList.isEmpty() && associatedViuUids?.isNotEmpty() == true) {
+                    // Show a message if relationships exist, but no one is online
+                    Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_no_internet),
+                                contentDescription = "Icon",
+                                modifier = Modifier.size(120.dp),
+                                colorFilter = ColorFilter.tint(Color.Gray)
+                            )
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Text(
+                                textAlign = TextAlign.Center,
+                                text = "Can't fetch your VIUs at the moment.\nTry again later?",
+                                style = MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Gray
+                                )
+                            )
+                        }
+                    }
+
+                } else if (usersList.isEmpty() && associatedViuUids?.isEmpty() == true) {
+                    // Show on empty
+                    Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_no_viu_stream),
+                                contentDescription = "Icon",
+                                modifier = Modifier.size(120.dp),
+                                colorFilter = ColorFilter.tint(Color.Gray)
+                            )
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Text(
+                                textAlign = TextAlign.Center,
+                                text = "No VIU currently registered.\nTry registering?",
+                                style = MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.Gray
+                                )
+                            )
+                        }
+                    }
+                } else {
+                    Box (modifier=Modifier
+                        .padding(horizontal=12.dp)) {
+                        // Show if all is good
+                        UserListView(
+                            users = usersList,
+                            onVideoCallClicked = { username ->
+                                Log.d("StreamCheck", "Video call clicked for $username")
+                                onVideoCall(username)
+                            },
+                            onAudioCallClicked = { username ->
+                                Log.d("StreamCheck", "Audio call clicked for $username")
+                                onAudioCall(username)
+                            }
                         )
-                    )
+                    }
                 }
             }
 
-        } else if (usersList.isEmpty() && associatedViuUids?.isEmpty() == true) {
-            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_no_viu_stream),
-                        contentDescription = "Icon",
-                        modifier = Modifier.size(120.dp),
-                        colorFilter = ColorFilter.tint(Color.Gray)
-                    )
-                    Spacer(modifier = Modifier.height(25.dp))
+            // Set alphabet scrollbar
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .fillMaxHeight()
+            ) {
+                alphabet.forEach { letter ->
                     Text(
-                        textAlign = TextAlign.Center,
-                        text = "No VIU currently registered.\nTry registering?",
-                        style = MaterialTheme.typography.bodyLarge.copy(
-                            fontWeight = FontWeight.Medium,
-                            color = Color.Gray
-                        )
+                        text = letter.toString(),
+                        modifier = Modifier.clickable {
+                            scope.launch {
+                                // TODO: Fix connection
+                                val targetIndex = yourSortedItemList.indexOfFirst {
+                                    it.name.startsWith(letter.toString(), ignoreCase = true)
+                                }
+                                if (targetIndex != -1) {
+                                    // Scroll to the item
+                                    listState.animateScrollToItem(targetIndex)
+                                }
+                            }
+                        }
                     )
                 }
             }
-        }
-        else {
-            UserListView(
-                users = usersList,
-                onVideoCallClicked = { username ->
-                    Log.d("StreamCheck", "Video call clicked for $username")
-                    onVideoCall(username)
-                },
-                onAudioCallClicked = { username ->
-                    Log.d("StreamCheck", "Audio call clicked for $username")
-                    onAudioCall(username)
-                }
-            )
         }
     }
 }
--- app/src/main/java/edu/capstone/navisight/common/webrtc/adapter/UserListViewAdapter.kt
+++ app/src/main/java/edu/capstone/navisight/common/webrtc/adapter/UserListViewAdapter.kt
@@ -4,43 +4,54 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import edu.capstone.navisight.R
+import edu.capstone.navisight.caregiver.model.Viu
 
 
 // Compose, compose, compose.
 
 @Composable
 fun UserListView (
-    users: SnapshotStateList<Triple<String, String, String>>,
+    users: SnapshotStateList<Triple<Viu, String, String>>,
     onVideoCallClicked: (String) -> Unit,
     onAudioCallClicked: (String) -> Unit
 ) {
     LazyColumn {
         items(users) { user ->
             UserListItem(
-                username = user.first,
+                viu = user.first,
                 uid = user.second,
                 status = user.third, // Update if necessary.
                 onVideoCallClicked = onVideoCallClicked,
@@ -52,11 +63,11 @@ fun UserListView (
 
 @Composable
 fun UserListItem(
+    viu: Viu,
     uid: String,
     status: String,
     onVideoCallClicked: (String) -> Unit,
     onAudioCallClicked: (String) -> Unit,
-    username: String
 ) {
     val statusColor = when (status) {
         "ONLINE" -> Color(0xFF4CAF50)  // light green
@@ -66,63 +77,124 @@ fun UserListItem(
     }
 
     val isOnline = status == "ONLINE"
+    val buttonContainerSize = 45.dp
+    val buttonContentSize = 25.dp
 
-    Column(
+    ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(12.dp)
-            .background(Color.White)
-            .border(1.dp, Color.LightGray, RoundedCornerShape(8.dp))
-            .padding(12.dp)
+            .padding(vertical=8.dp),
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = 2.dp
+        ),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = Color.White
+        )
     ) {
-
-        Text(
-            text = username,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-            color = colorResource(id=R.color.dark_violet)
-        )
-        Text(text=uid, fontSize=14.sp)
-
-        Text(
-            text = when (status) {
-                "ONLINE" -> "Online"
-                "OFFLINE" -> "Offline"
-                "IN_CALL" -> "In Call"
-                else -> "Unknown"
-            },
-            color = statusColor,
-            fontSize = 14.sp,
-            modifier = Modifier.padding(top = 4.dp, bottom = 8.dp)
-        )
-
-        if (isOnline) {
-            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                Button(onClick = { onVideoCallClicked(uid) }) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_camera_on),
-                            contentDescription = "Video Call Icon",
-                            modifier = Modifier.size(20.dp)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f, fill = false)
+            ) {
+                Row {
+                    AsyncImage(
+                        model = viu.profileImageUrl,
+                        contentDescription = "Profile Image",
+                        fallback = painterResource(R.drawable.default_profile),
+                        error = painterResource(R.drawable.default_profile),
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .shadow(10.dp, RoundedCornerShape(12.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        modifier = Modifier.weight(1f, fill = false)
+                    ) {
+                        Text(
+                            text = "${viu.firstName} ${viu.lastName}",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            color = colorResource(id = R.color.dark_violet)
                         )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text("Video Call")
+                        Text(
+                            text = uid,
+                            fontSize = 10.sp,
+                            color = Color.Gray
+                        )
+                        Text(
+                            text = when (status) {
+                                "ONLINE" -> "Online"
+                                "OFFLINE" -> "Offline"
+                                "IN_CALL" -> "In Call"
+                                else -> "Unknown"
+                            },
+                            color = statusColor,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(top = 4.dp, bottom = 8.dp)
+                        )
                     }
                 }
+            }
 
-                // --- Audio Call Button ---
-                Button(onClick = { onAudioCallClicked(uid) }) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_call),
-                            contentDescription = "Audio Call Icon",
-                            modifier = Modifier.size(20.dp)
+            if (isOnline) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp) // Space between buttons
+                ) {
+                    // Set video call button
+                    Button(
+                        onClick = { onVideoCallClicked(uid) },
+                        shape = CircleShape,
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(buttonContainerSize),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(R.color.light_blue),
+                            contentColor = colorResource(R.color.blue)
                         )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text("Audio Call")
+
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_video_call_outline),
+                                contentDescription = "Video Call Icon",
+                                modifier = Modifier.size(buttonContentSize)
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                        }
+                    }
+
+                    // Set audio call button
+                    Button(
+                        onClick = { onAudioCallClicked(uid) },
+                        shape = CircleShape,
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(buttonContainerSize),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(R.color.light_purple),
+                            contentColor = colorResource(R.color.royal_purple)
+                        )
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_call_outline),
+                                contentDescription = "Audio Call Icon",
+                                modifier = Modifier.size(buttonContentSize)
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                        }
                     }
                 }
             }
         }
+        Spacer(modifier = Modifier.height(4.dp))
     }
+
 }
